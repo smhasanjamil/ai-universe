5f377a21233737c383fc809dd34f2757c55bae78
smhasanjamil <smhasanjamil23@gmail.com>
2023-03-03 10:06:09 +0600

    Fetch all data from api

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const loadData = () =>{
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res => res.json())
+    .then(data => console.log(data.data.tools))
+}
+
+loadData();
