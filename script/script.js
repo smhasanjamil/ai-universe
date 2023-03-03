f7c7fd1b36156b018181dfe16668aa2ddfeeeba6
smhasanjamil <smhasanjamil23@gmail.com>
2023-03-03 10:10:59 +0600

    Display data into another function

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 const loadData = () =>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data => console.log(data.data.tools))
+    .then(data => displayData(data.data.tools))
 }
 
 loadData();
+
+const displayData = (value) =>{
+    console.log(value);
+}
