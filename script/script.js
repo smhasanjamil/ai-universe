0622cbd2e479640624bff31ff5a248ba9bbf2251
smhasanjamil <smhasanjamil23@gmail.com>
2023-03-03 10:29:30 +0600

    Dynamically fetch data and append child to div

--- script/script.js
+++ script/script.js
@@ -16,7 +16,20 @@ const displayData = (datas) => {
         const container = document.getElementById('all-data');
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML = ``;
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        `;
+
+        container.appendChild(div);
 
     })
 }
