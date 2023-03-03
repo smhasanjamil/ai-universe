2ea14077e746945d724fb7309350d9c049680199
smhasanjamil <smhasanjamil23@gmail.com>
2023-03-03 10:26:04 +0600

    Div created for display data and added class

--- script/script.js
+++ script/script.js
@@ -1,11 +1,22 @@
-const loadData = () =>{
+//Fetch All Data from api
+const loadData = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
-    .then(res => res.json())
-    .then(data => displayData(data.data.tools))
+        .then(res => res.json())
+        .then(data => displayData(data.data.tools))
 }
 
 loadData();
 
-const displayData = (value) =>{
-    console.log(value);
+//Display Data from Api
+const displayData = (datas) => {
+    datas.forEach((value) => {
+        console.log(value);
+        const { id, name, description, image, published_in, features } = value;
+
+        const container = document.getElementById('all-data');
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = ``;
+
+    })
 }
